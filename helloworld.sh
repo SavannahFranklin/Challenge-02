ac02e471c19daf089a38f9e7a6d7cf99732f06ac
Savannah Franklin <86566196+SavannahFranklin@users.noreply.github.com>
2021-07-09 15:01:41 -0500

    Add files via upload

---
+++ helloworld.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+AUTHOR=Savannah
+DATE=07/09/21
+var1=Author
+var2=Date
+var3=Variables
+
+echo "Script Name: $0
+Author: $AUTHOR
+Date of last revision: $DATE
+Declaration of variables: $var1, $var2, and $var3
+End"
